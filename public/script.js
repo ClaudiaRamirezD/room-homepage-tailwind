0c980da7c4b9cd3a19943c93094d3d6273121101
Claudia Ramirez <clau8812@gmail.com>
2025-04-01 21:06:53 -0600

    Finished

--- public/script.js
+++ public/script.js
@@ -4,6 +4,10 @@ const menuBtn = document.querySelector("#menu-open");
 const closeBtn = document.querySelector("#menu-close");
 const navbar = document.querySelector(".header__nav-mobile");
 const backdrop = document.querySelector("#backdrop");
+const prevBtn = document.querySelector(".carousel-control-prev");
+const nextBtn = document.querySelector(".carousel-control-next");
+const pictures = document.querySelectorAll(".main-img picture");
+const texts = document.querySelectorAll(".hero-text article");
 
 //Abrir menu
 menuBtn.addEventListener("click", () => {
@@ -17,5 +21,33 @@ closeBtn.addEventListener("click", () => {
     navbar.classList.remove("hidden");
     backdrop.classList.add("hidden"); // Hide the menu again
     backdrop.classList.remove("block");
-
 });
+
+// Carousel
+
+let currentIndex = 0;
+const totalSlides = pictures.length;
+
+function updateCarousel(index) {
+    //Hide all images and text
+    pictures.forEach((pic) => pic.classList.add("hidden"));
+    texts.forEach((text) => text.classList.add("hidden"));
+
+    //Show current images and text
+    pictures[index].classList.remove("hidden");
+    texts[index].classList.remove("hidden");
+}
+
+//Handle buttons click
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateCarousel(currentIndex);
+})
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides; 
+    updateCarousel(currentIndex);
+});
+
+// Initialize by showing the first slide
+updateCarousel(currentIndex);
